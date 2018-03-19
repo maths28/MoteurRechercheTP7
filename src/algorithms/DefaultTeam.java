1c07d8b03619157afdfa88ffcf67659f32bffe65
mathieu <mathieu.boye28@gmail.com>
2018-03-19 10:19:05 +0100

    Commit

---
+++ src/algorithms/DefaultTeam.java
@@ -0,0 +1,102 @@
+package algorithms;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class DefaultTeam {
+
+    public static final int DISTANCE_POINT = 55;
+
+    public ArrayList<Point> calculDominatingSet(ArrayList<Point> points, int edgeThreshold) {
+        //REMOVE >>>>>
+        ArrayList<Point> result = (ArrayList<Point>) points.clone();
+        for (int i = 0; i < points.size(); i++){
+            result.remove(0);
+        }
+        // if (false) result = readFromFile("output0.points");
+        // else saveToFile("output",result);
+        //<<<<< REMOVE
+
+        return result;
+    }
+
+    private List<Point> findAllPointsRelied(Point point, List<Point> allPoints) {
+        List<Point> result = new ArrayList<>();
+        for (Point p : allPoints) {
+            if (!p.equals(point) && p.distance(p) <= DISTANCE_POINT) {
+                result.add(p);
+            }
+        }
+        return result;
+
+    }
+
+    //FILE PRINTER
+    private void saveToFile(String filename, ArrayList<Point> result) {
+        int index = 0;
+        try {
+            while (true) {
+                BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(filename + Integer.toString(index) + ".points")));
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    System.err.println("I/O exception: unable to close " + filename + Integer.toString(index) + ".points");
+                }
+                index++;
+            }
+        } catch (FileNotFoundException e) {
+            printToFile(filename + Integer.toString(index) + ".points", result);
+        }
+    }
+
+    private void printToFile(String filename, ArrayList<Point> points) {
+        try {
+            PrintStream output = new PrintStream(new FileOutputStream(filename));
+            int x, y;
+            for (Point p : points)
+                output.println(Integer.toString((int) p.getX()) + " " + Integer.toString((int) p.getY()));
+            output.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("I/O exception: unable to create " + filename);
+        }
+    }
+
+    //FILE LOADER
+    private ArrayList<Point> readFromFile(String filename) {
+        String line;
+        String[] coordinates;
+        ArrayList<Point> points = new ArrayList<Point>();
+        try {
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(filename))
+            );
+            try {
+                while ((line = input.readLine()) != null) {
+                    coordinates = line.split("\\s+");
+                    points.add(new Point(Integer.parseInt(coordinates[0]),
+                            Integer.parseInt(coordinates[1])));
+                }
+            } catch (IOException e) {
+                System.err.println("Exception: interrupted I/O.");
+            } finally {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    System.err.println("I/O exception: unable to close " + filename);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Input file not found.");
+        }
+        return points;
+    }
+}
